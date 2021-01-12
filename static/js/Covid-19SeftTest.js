a788542d869c896f6abd09d716df5ef18fb8c75c
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2021-01-12 11:15:38 -0500

    Updater Ver
    The progress bar doesnt function properly

--- static/js/Covid-19SeftTest.js
+++ static/js/Covid-19SeftTest.js
@@ -1,16 +1,31 @@
 //Determined from Machine Learning
-
+let testResult = false;
 
 function displayResult(){
-    if(testResult == 1)
+
+    let progressBar = document.getElementById("progBar");
+            document.getElementById("pBar").style.display = "block";
+
+            let i = 2
+            function progress(){
+                if(i <100){
+                    i++;
+                    progressBar.style.width = i + "%";
+                }
+                setTimeout("progress()", 100);
+            }
+            progress();
+
+    if(testResult){
         document.getElementById("pResult").style.display = "block";
-    else
+    }
+    else{
         document.getElementById("nResult").style.display = "block";
+    }
 }
 
 let el = document.getElementById("file");
 el.addEventListener('input', displayResult);
-
 
 
 
